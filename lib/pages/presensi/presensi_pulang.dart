583b493b95d6124f01c6890a2891b19634ecff1c
dewiokta <dewiokta48@gmail.com>
2022-06-30 19:42:44 +0700

    fixing presensi

--- lib/pages/presensi/presensi_pulang.dart
+++ lib/pages/presensi/presensi_pulang.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter_attendance/maindrawer.dart';
 import 'package:flutter_attendance/model/presensidatang_model.dart';
 import 'package:flutter_attendance/model/presensipulang_model.dart';
+import 'package:flutter_attendance/network/api_service.dart';
+import 'package:flutter_attendance/pages/login/blocs/auth_event.dart';
+import 'package:flutter_attendance/pages/login/blocs/auth_state.dart';
 import 'package:flutter_attendance/pages/profile/bloc/profile_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
@@ -12,7 +17,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../login/blocs/Auth_bloc.dart';
-import '../../dio_service/service.dart';
 import '../../theme.dart';
 
 class PresensiPulang extends StatefulWidget {
@@ -26,21 +30,22 @@ class PresensiPulang extends StatefulWidget {
 }
 
 class _PresensiPulangState extends State<PresensiPulang> {
-  PresensiDatangModel? presensi;
+  PresensiPulangModel? presensi;
   AuthBloc get _authBloc => widget.authBloc;
   ProfileBloc get _profileBloc => widget.profileBloc;
-
-  var _latitude = "";
-  var _longtitude = "";
-  var _address = "";
-  var _status = "On Process";
-  int _user_id = 0;
-  var _foto_datang = "coba.png";
 
   // getID(String id) {
   //   _user_id = id;
   //   // print(_user_id);
   // }
+
+  var _latitude = "";
+  var _longtitude = "";
+  var _address = "";
+  var _status = "On Process";
+  // var _user_id = "";
+  var _foto_datang = "";
+  // var image = "";
 
   File? _image;
   final imagePicker = ImagePicker();
@@ -48,27 +53,43 @@ class _PresensiPulangState extends State<PresensiPulang> {
   Future<void> _updatePosition() async {
     Position pos = await _determinePosition();
     List pm = await placemarkFromCoordinates(pos.latitude, pos.longitude);
-    // final image = await imagePicker.getImage(source: ImageSource.camera);
+    final image = await imagePicker.getImage(source: ImageSource.camera);
     // ignore: unused_element
     setState(() {
       _latitude = pos.latitude.toString();
       _longtitude = pos.longitude.toString();
       _address = pm[0].toString();
-      // _image = File(image!.path);
+      _image = File(image!.path);
       _status;
-      _foto_datang;
-      // _user_id = (getID(_user_id));
+      // _foto_datang;
+      // _user_id = getID(_user_id);
     });
+    // print(_longtitude);
+    // print(_user_id);
+    // print(_latitude);
+    // print(_status);
+    // print(_image);
   }
 
   Future<void> _submit() async {
-    PresensiDatangModel? result = await Services.createPresensiDatang(
-        4, -1.394829384, 212.214134, "abs.jpg", "On Process");
+    PresensiPulangModel? result = await ApiService()
+        .createPresensiPulang(_latitude, _longtitude, _image, _status);
+    print(_longtitude);
+    print(_latitude);
+    print(_status);
+    print(_image);
     if (result != null) {
       setState(() {
         presensi = result;
+        // print(presensi);
       });
     }
+    CoolAlert.show(
+      context: context,
+      type: CoolAlertType.success,
+      text: 'Presensi Sukses! Anda tidak perlu presensi lagi !',
+      autoCloseDuration: Duration(seconds: 2),
+    );
   }
 
   Future<Position> _determinePosition() async {
@@ -112,54 +133,73 @@ class _PresensiPulangState extends State<PresensiPulang> {
       drawer: Drawer(
         child: MainDrawer(authBloc: _authBloc, profileBloc: _profileBloc),
       ),
-      body: ListView(
-        children: [
-          Column(children: [
-            Center(
-              child: _image == null
-                  ? Text("Klik Icon Kamera untuk Presensi")
-                  : Image.file(_image!, width: 250),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ]),
-          Column(
-            children: [
-              Padding(padding: EdgeInsets.only(top: 40)),
-              menuAccount("Longtitude", _longtitude),
-              Padding(padding: EdgeInsets.only(top: 20)),
-              menuAccount("Latitude", _latitude),
-              Padding(padding: EdgeInsets.only(top: 20)),
-              menuAccount("Address", _address),
-              Padding(padding: EdgeInsets.only(top: 20)),
-              menuAccount("Status", _status),
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            width: 0.5,
-            height: 45,
-            child: TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 120, 100, 210),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+      body: BlocBuilder<AuthBloc, AuthState>(
+          bloc: _authBloc,
+          builder: (context, state) {
+            if (state is AuthHasToken) {
+              _authBloc.add(GetDataWithToken(state.token));
+              return Container();
+            }
+            if (state is AuthData) {
+              // var _id = state.id.toString();
+              // print(_id);
+              // getID(_id);
+              return Container(
+                child: ListView(
+                  children: [
+                    Column(children: [
+                      Center(
+                        child: _image == null
+                            ? Text("Klik Icon Kamera untuk Presensi")
+                            : Image.file(_image!, width: 250),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ]),
+                    Column(
+                      children: [
+                        Padding(padding: EdgeInsets.only(top: 40)),
+                        menuAccount("Longtitude", _longtitude),
+                        Padding(padding: EdgeInsets.only(top: 20)),
+                        menuAccount("Latitude", _latitude),
+                        Padding(padding: EdgeInsets.only(top: 20)),
+                        menuAccount("Address", _address),
+                        Padding(padding: EdgeInsets.only(top: 20)),
+                        menuAccount("Status", _status),
+                        // Padding(padding: EdgeInsets.only(top: 20)),
+                        // menuAccount("User id", _user_id),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: 0.5,
+                      height: 45,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 120, 100, 210),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        onPressed: _submit,
+                        child: const Text(
+                          "Simpan",
+                          style: TextStyle(
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              onPressed: _submit,
-              child: const Text(
-                "Simpan",
-                style: TextStyle(
-                  color: Color(0xffffffff),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+              );
+            }
+            return Container();
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: _updatePosition,
         backgroundColor: const Color.fromARGB(255, 120, 100, 210),
--- lib/pages/riwayat/riwayat.dart
+++ lib/pages/riwayat/riwayat.dart
@@ -6,6 +6,8 @@ import 'package:flutter_attendance/pages/profile/bloc/profile_bloc.dart';
 import 'package:flutter_attendance/pages/riwayat/list_riwayat.dart';
 import 'package:getwidget/getwidget.dart';
 
+import '../../model/riwayatdatang_model.dart';
+import '../../network/api_service.dart';
 import '../login/blocs/Auth_bloc.dart';
 import '../login/blocs/auth_repository.dart';
 
@@ -20,16 +22,43 @@ class RiwayatWidget extends StatefulWidget {
 }
 
 class _RiwayatWidgetState extends State<RiwayatWidget> {
-  DateTimeRange dateRange =
-      DateTimeRange(start: DateTime(2022, 2, 15), end: DateTime(2022, 3, 15));
   final AuthRepository authRepository = AuthRepository();
   AuthBloc get _authBloc => widget.authBloc;
   ProfileBloc get _profileBloc => widget.profileBloc;
+  late BuildContext context;
+  late ApiService apiService;
   @override
-  Widget build(BuildContext context) {
-    final start = dateRange.start;
-    final end = dateRange.end;
+  void initState() {
+    super.initState();
+    apiService = ApiService();
+  }
 
+  Widget build(BuildContext context) {
+    this.context = context;
+    return SafeArea(
+      child: FutureBuilder(
+        future: apiService.getDataRiwayatDatang(),
+        builder: (BuildContext context,
+            AsyncSnapshot<RiwayatDatangResponse> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                  "Something wrong with message: ${snapshot.error.toString()}"),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            List<RiwayatDatangDataResponse>? datariwayat = snapshot.data?.data;
+            return _buildListView(datariwayat!);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildListView(List<RiwayatDatangDataResponse> datariwayat) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Riwayat Presensi"),
@@ -42,87 +71,81 @@ class _RiwayatWidgetState extends State<RiwayatWidget> {
         children: [
           Container(
             margin: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Row(
-                  children: const [
-                    Text(
-                      "Detail Riwayat Mingguan",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SizedBox(
-                      width: 140,
-                      child: GFButton(
-                        onPressed: pickDate,
-                        text: "${start.year}/${start.month}/${start.day}",
-                        shape: GFButtonShape.square,
-                        type: GFButtonType.outline2x,
-                        color: kPrimaryColor,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                RiwayatDatangDataResponse state = datariwayat[index];
+                return Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Row(
+                            children: const [
+                              Text(
+                                "Detail Riwayat Mingguan",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: const <Widget>[
+                                  Image(
+                                    image:
+                                        AssetImage("assets/images/riwayat.png"),
+                                    height: 50,
+                                  )
+                                ],
+                              ),
+                              Column(
+                                children: <Widget>[
+                                  Text(
+                                    state.waktu,
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: const <Widget>[
+                                  Text(
+                                    "Datang",
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: <Widget>[
+                                  Text(
+                                    state.status,
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: const <Widget>[
+                                  Image(
+                                    image: AssetImage(
+                                        "assets/images/centang-riwayat.png"),
+                                    height: 20,
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                    const SizedBox(
-                      child: Icon(Icons.arrow_right),
-                    ),
-                    SizedBox(
-                      width: 140,
-                      child: GFButton(
-                        onPressed: pickDate,
-                        text: "${end.year}/${end.month}/${end.day}",
-                        shape: GFButtonShape.square,
-                        type: GFButtonType.outline2x,
-                        color: kPrimaryColor,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: const [
-                    Text(
-                      "see all",
-                      style: TextStyle(fontSize: 15, color: kPrimaryColor),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                const ListRiwayat(),
-                SizedBox(height: 20),
-                const ListRiwayat(),
-                SizedBox(height: 20),
-                const ListRiwayat(),
-                SizedBox(height: 20),
-                const ListRiwayat(),
-                SizedBox(height: 20),
-                const ListRiwayat(),
-                SizedBox(height: 20),
-                const ListRiwayat(),
-                SizedBox(height: 20),
-              ],
+                  ),
+                );
+              },
+              itemCount: datariwayat.length,
             ),
           ),
         ],
       ),
     );
-  }
-
-  Future pickDate() async {
-    DateTimeRange? newDateRange = await showDateRangePicker(
-      context: context,
-      initialDateRange: dateRange,
-      firstDate: DateTime(1900),
-      lastDate: DateTime(2100),
-    );
-
-    if (newDateRange == null) return; //press X
-
-    setState(() => dateRange = newDateRange);
   }
 }
